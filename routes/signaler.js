39d64db510a31a4e0c94f20c59fb7069b4a60736
Alexis Nootens <screams240@gmail.com>
2015-03-19 22:23:41 +0100

    Ajout de mongodb et assert

--- routes/signaler.js
+++ routes/signaler.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('signaler', { title: '3' });
+// Connection URL
+var url = 'mongodb://10.211.55.21:27017/routr';
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to database in signaler");
+
+  router.get('/', function(req, res, next) {
+  	res.render('signaler', db);
+	});
+
+  db.close();
 });
 
 module.exports = router;
