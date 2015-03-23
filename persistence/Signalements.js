1db53d293343b6835f3743a41cf03a64f531a653
jeekiii <iii.iii.iii.3x3@gmail.com>
2015-03-23 14:18:24 +0100

    signalements updated

--- persistence/Signalements.js
+++ persistence/Signalements.js
@@ -5,46 +5,38 @@ module.exports = function Signalements(db) {
     var signalements = db.collection("signalements");
 
     return {
-        addPost: function(date_added, date_latest_upvote, zone, direction, type, vote, author) {
+        addSignalement: function(date_added, date_latest_upvote, zone, direction, type, vote, author, done) {
             var entry = {
                 date_added: new Date(),
                 date_latest_upvote: new Date(),
                 zone: zone,
                 direction: direction,
-                type, type,
-                vote, vote,
+                type: type,
+                vote: 1,
                 author: author, //put the current user here!
             };
-            posts.insert(entry, function (error, result) {
+            signalements.insert(entry, function (error, result) {
                 if (error) return done(error, null);
-                console.log("DB: inserted post " + entry.title);
+                console.log("DB: inserted signalements ");
                 return done(error, permalink);
             });
         },
-        getPosts: function(count, done) {
-            posts.find().sort('date', -1).limit(count).toArray(function(error, items) {
+        getSignalements: function(count, done) {
+            posts.find().sort('vote').limit(count).toArray(function(error, items) {
+                if (error) return done(error, null);
+                console.log("Found " + items.length + " signalements");
+                return done(error, items);
+            });
+        },
+        getPostsByZone: function(zone, count, done) {
+            posts.find({ zone : zone }).sort('vote').limit(num).toArray(function(error, items) {
                 if (error) return done(error, null);
                 console.log("Found " + items.length + " posts");
                 return done(error, items);
             });
         },
-        getPostsByTag: function(tag, count, done) {
-            posts.find({ tags : tag }).sort('date', -1).limit(num).toArray(function(error, items) {
-                if (error) return done(error, null);
-                console.log("Found " + items.length + " posts");
-                return done(error, items);
-            });
-        },
-        getPostByPermalink: function(permalink, done) {
-            posts.findOne({'permalink': permalink}, function(error, post) {
-                if (error) return done(error, null);
-                return done(error, post);
-            });
-        },
-        addComment: function(permalink, name, email, body, done) {
-            var comment = { author: name, body: body };
-            if (email) comment.email = email;
-            posts.update({permalink: permalink}, {'$push': {comments: comment}}, function(error, count) {
+        addVote: function(user, vote, done) {
+            signalements.update({permalink: permalink}, {'$push': {comments: comment}}, function(error, count) {
                 if (error) return done(error, null);
                 return done(error, count);
             });
