103e743b58a8e06b0a3ee65fe89afdccca219c27
jeekiii <iii.iii.iii.3x3@gmail.com>
2015-03-25 02:38:26 +0100

    boutons pour mettre un marker sur la liste

--- views/js/map.js
+++ views/js/map.js
@@ -1,25 +1,25 @@
 
-
+var map;
+var geocoder;
 function initialize() {
+    geocoder= new google.maps.Geocoder();
     var mapOptions = {
         zoom: 8,
         center: new google.maps.LatLng(51.00, 5.00)
     };
 
-    var map = new google.maps.Map(document.getElementById('map-canvas'),
+    map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
 }
 
 function loadScript() {
     var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' + '&signed_in=true&callback=initialize';
+    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' + '&signed_in=true&callback=initialize&region=BE';
     document.body.appendChild(script);
 }
 
-function codeAddress() {
-    var address = document.getElementById("address").value;
-    var markerType = document.getElementById("type").value;
+function codeAddress(address) {
     geocoder.geocode( { 'address': address}, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK)
         {
