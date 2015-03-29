56209d2de59dbee7d209296b95c165b708150474
Alexis Nootens <screams240@gmail.com>
2015-03-29 17:31:37 +0200

    Ajustement mineures

--- views/js/map.js
+++ views/js/map.js
@@ -5,7 +5,7 @@ function initialize() {
     geocoder = new google.maps.Geocoder();
     var mapOptions = {
         zoom: 8,
-        center: new google.maps.LatLng(51.00, 5.00),
+        center: new google.maps.LatLng(50.501079,4.4764595),
         styles: [{"featureType":"all","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"visibility":"on"},{"color":"#f3f4f4"}]},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"weight":0.9},{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#83cead"}]},{"featureType":"road","elementType":"all","stylers":[{"visibility":"on"},{"color":"#ffffff"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"on"},{"color":"#fee379"}]},{"featureType":"road.arterial","elementType":"all","stylers":[{"visibility":"on"},{"color":"#fee379"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"color":"#7fc8ed"}]}]
     };
 
